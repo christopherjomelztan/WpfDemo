d845ed7fe4c6d54672c2ba605bfbb57afe2f772e
christopherjomelztan <christopherjomelztan@gmail.com>
2017-04-12 17:28:53 +0800

    added test

---
+++ WpfDemoTests/MainViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo.Tests
+{
+    [TestClass()]
+    public class MainViewModelTests
+    {
+        [TestMethod()]
+        public void PropertyNotifiedRaisedWhenFirstNameChanges()
+        {
+            var eventcalled = false;
+            var vm = new MainViewModel();
+            vm.PropertyChanged += (sender, args) => { eventcalled = true; };
+            vm.FirstName = "";
+            vm.LastName = "";
+
+            Assert.IsTrue(eventcalled);
+        }
+
+        [TestMethod()]
+        public void OnPropertyChangedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MainViewModelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
